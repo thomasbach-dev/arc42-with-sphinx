8f3205183c5c924d5ae59ee86ecc9e73ec4602b0
Thomas Bach <info@thomasbach.dev>
2024-01-27 21:21:04 +0100

    refactor(nix): restructure flake to be a bit more ergonomic

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system} = rec {
+        hello = pkgs.hello;
+        default = hello;
+      };
+    };
 }
